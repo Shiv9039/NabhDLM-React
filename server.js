841ef393576e4ed22532bff81c730190ae712ea0
Shivendra Patel <64832314+Shiv9039@users.noreply.github.com>
2022-08-20 17:53:07 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const jsonServer = require('json-server');
 const server = jsonServer.create();
 const router = jsonServer.router('./db.json');
+const cors = require('cors');
 const middlewares = jsonServer.defaults({
   static: './build'
 });
